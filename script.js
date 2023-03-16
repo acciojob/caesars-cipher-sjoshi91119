3f30000c498b506813a98d11c55c3a5eaca657a7
Shubham Joshi <115713904+sjoshi91119@users.noreply.github.com>
2023-03-16 17:36:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,7 +13,12 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
+	const string = encodedStr;
+	const arr = [];
+ for(let i = 0;i<string.length;i++){
+	string.charAt(i) = string.charAt(i+13);
+	 arr.push(string[i]);
+ }
   return ;//return decodedArr
 }
 
